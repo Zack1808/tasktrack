2d47cf99efa558a47730f429386b4bfb47661817
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-05-27 06:29:42 +0200

    Test added to check for Link tag and anchor tag when specific props are provided

--- tests/components/Button.test.tsx
+++ tests/components/Button.test.tsx
@@ -1,22 +1,23 @@
 import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Button from "../../src/components/Button";
 import "@testing-library/jest-dom/vitest";
 
 describe("Button component", () => {
-  it("renders with 'contained' variant", () => {
+  it("should render with 'contained' variant", () => {
     render(<Button variant="contained">Contained Variant</Button>);
     const button = screen.getByRole("button", { name: "Contained Variant" });
     expect(button).toHaveAttribute("data-variant", "contained");
   });
 
-  it("renders with 'outlined' variant", () => {
+  it("should render with 'outlined' variant", () => {
     render(<Button variant="outlined">Outlined Variant</Button>);
     const button = screen.getByRole("button", { name: "Outlined Variant" });
     expect(button).toHaveAttribute("data-variant", "outlined");
   });
 
-  it("renders with 'text' variant", () => {
+  it("should render with 'text' variant", () => {
     render(
       <Button variant="text" name="text-button">
         Text Variant
@@ -24,5 +25,33 @@ describe("Button component", () => {
     );
     const button = screen.getByRole("button", { name: "Text Variant" });
     expect(button).toHaveAttribute("data-variant", "text");
+  });
+
+  it("should render a Link button when link prop is provided", () => {
+    render(
+      <MemoryRouter>
+        <Button variant="text" link="/home">
+          Button with Link component
+        </Button>
+      </MemoryRouter>
+    );
+    const link = screen.getByRole("link", {
+      name: "Button with Link component",
+    });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "/home");
+    expect(link).not.toHaveAttribute("target", "_blank");
+  });
+
+  it("should render an anchor tag when link and target are provided", () => {
+    render(
+      <Button variant="text" link="https://example.com" target="_blank">
+        Button with Anchor tag
+      </Button>
+    );
+    const anchor = screen.getByRole("link", { name: "Button with Anchor tag" });
+    expect(anchor).toBeInTheDocument();
+    expect(anchor).toHaveAttribute("href", "https://example.com");
+    expect(anchor).toHaveAttribute("target", "_blank");
   });
 });
